833db643edc3b3f01398db92309f7735f6b8a88a
Hakky54 <hakangoudberg@hotmail.com>
2019-01-15 08:54:12 +0100

    Added pretty html report

--- client/src/test/java/nl/altindag/client/ClientRunnerIT.java
+++ client/src/test/java/nl/altindag/client/ClientRunnerIT.java
@@ -1,14 +1,14 @@
 package nl.altindag.client;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = "pretty",
-        features = "src/test/resources/features",
-        glue = "nl.altindag.client.stepdefs")
+@CucumberOptions(plugin = { "pretty", "json:target/test-report/report.json" },
+                 features = "src/test/resources/features",
+                 glue = "nl.altindag.client.stepdefs")
 public class ClientRunnerIT {
 
 }
