94bd3e1b9544531d06ecb51df8323786467a5fad
Hakky54 <hakangoudberg@hotmail.com>
2019-07-22 07:53:46 +0200

    Added option for detailed logging of the certificate

--- server/src/main/java/nl/altindag/server/aspect/LogCertificateAspect.java
+++ server/src/main/java/nl/altindag/server/aspect/LogCertificateAspect.java
@@ -1,20 +1,21 @@
 package nl.altindag.server.aspect;
 
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import javax.security.auth.x500.X500Principal;
+
+import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.security.Principal;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 @Aspect
 @Configuration
@@ -24,14 +25,31 @@ public class LogCertificateAspect {
     private static final Logger LOGGER = LogManager.getLogger(LogCertificateAspect.class);
     private static final String KEY_CERTIFICATE_ATTRIBUTE = "javax.servlet.request.X509Certificate";
 
-    @Before("@annotation(nl.altindag.server.aspect.LogCertificate)")
-    public void logCertificateIfPresent(JoinPoint joinPoint) {
-        getCertificatesFromRequest()
+    @Before("@annotation(logCertificate)")
+    public void logCertificateIfPresent(LogCertificate logCertificate) {
+        Optional<String> certificateDetails;
+        if (logCertificate.detailed()) {
+            certificateDetails = getPublicCertificate();
+        } else {
+            certificateDetails = getSubjectDistinguishedName();
+        }
+
+        certificateDetails.ifPresent(certificate -> LOGGER.info("Received the following certificate details: " + certificate));
+    }
+
+    private Optional<String> getPublicCertificate() {
+        return getCertificatesFromRequest()
                 .map(Arrays::stream)
                 .flatMap(Stream::findFirst)
-                .map(X509Certificate::getSubjectDN)
-                .map(Principal::getName)
-                .ifPresent(certificate -> LOGGER.info("Received the following certificate details: " + certificate));
+                .map(ConvertUtils::convert);
+    }
+
+    private Optional<String> getSubjectDistinguishedName() {
+        return getCertificatesFromRequest()
+                .map(Arrays::stream)
+                .flatMap(Stream::findFirst)
+                .map(X509Certificate::getSubjectX500Principal)
+                .map(X500Principal::getName);
     }
 
     private Optional<X509Certificate[]> getCertificatesFromRequest() {
@@ -39,4 +57,5 @@ public class LogCertificateAspect {
                 .getRequest()
                 .getAttribute(KEY_CERTIFICATE_ATTRIBUTE));
     }
+
 }
