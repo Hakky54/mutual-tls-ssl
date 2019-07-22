7085652dfeca7245591bd110a5eb48f942b08608
Hakky54 <hakangoudberg@hotmail.com>
2019-07-22 07:51:38 +0200

    Added option for detailed logging of the certificate

--- server/src/main/java/nl/altindag/server/aspect/LogCertificate.java
+++ server/src/main/java/nl/altindag/server/aspect/LogCertificate.java
@@ -9,4 +9,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface LogCertificate {
 
+    boolean detailed();
+
 }
--- server/src/main/java/nl/altindag/server/controller/HelloWorldController.java
+++ server/src/main/java/nl/altindag/server/controller/HelloWorldController.java
@@ -1,15 +1,16 @@
 package nl.altindag.server.controller;
 
-import nl.altindag.server.aspect.LogCertificate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import nl.altindag.server.aspect.LogCertificate;
 
 @Controller
 public class HelloWorldController {
 
     @GetMapping("/api/hello")
-    @LogCertificate
+    @LogCertificate(detailed = true)
     public ResponseEntity hello() {
         return ResponseEntity.ok("Hello");
     }
