b4640f32c0f4d93f88d51eda2859590cb3d36397
Hakan Altindag <hakangoudberg@hotmail.com>
2019-09-28 15:35:23 +0200

    Added option to view which files are deleted

--- configure-mutual-tls.sh
+++ configure-mutual-tls.sh
@@ -3,26 +3,28 @@
 cleanUpExistingCertificatesAndKeystores() {
     echo 'Cleaning up existing certificates and keystores'
 
-    rm -f client/src/test/resources/client.cer
-    rm -f client/src/test/resources/client.csr
-    rm -f client/src/test/resources/client.p12
-    rm -f client/src/test/resources/client-private.key
-    rm -f client/src/test/resources/client-signed.cer
-    rm -f client/src/test/resources/client-signed.p12
-    rm -f client/src/test/resources/identity.jks
-    rm -f client/src/test/resources/truststore.jks
-    rm -f root-ca/root-ca.key
-    rm -f root-ca/root-ca.p12
-    rm -f root-ca/root-ca.pem
-    rm -f root-ca/root-ca.srl
-    rm -f server/src/main/resources/identity.jks
-    rm -f server/src/main/resources/server.cer
-    rm -f server/src/main/resources/server.csr
-    rm -f server/src/main/resources/server.p12
-    rm -f server/src/main/resources/server-private.key
-    rm -f server/src/main/resources/server-signed.cer
-    rm -f server/src/main/resources/server-signed.p12
-    rm -f server/src/main/resources/truststore.jks
+    rm -fv client/src/test/resources/client.cer
+    rm -fv client/src/test/resources/client.csr
+    rm -fv client/src/test/resources/client.p12
+    rm -fv client/src/test/resources/client-private.key
+    rm -fv client/src/test/resources/client-signed.cer
+    rm -fv client/src/test/resources/client-signed.p12
+    rm -fv client/src/test/resources/identity.jks
+    rm -fv client/src/test/resources/truststore.jks
+    rm -fv root-ca/root-ca.key
+    rm -fv root-ca/root-ca.p12
+    rm -fv root-ca/root-ca.pem
+    rm -fv root-ca/root-ca.srl
+    rm -fv server/src/main/resources/identity.jks
+    rm -fv server/src/main/resources/server.cer
+    rm -fv server/src/main/resources/server.csr
+    rm -fv server/src/main/resources/server.p12
+    rm -fv server/src/main/resources/server-private.key
+    rm -fv server/src/main/resources/server-signed.cer
+    rm -fv server/src/main/resources/server-signed.p12
+    rm -fv server/src/main/resources/truststore.jks
+
+    echo 'Finished cleanup'
 }
 
 createCertificates() {
