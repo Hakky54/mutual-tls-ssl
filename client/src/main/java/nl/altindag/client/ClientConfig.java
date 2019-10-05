23f46fcda6fac76257350dc226fac1811bb27605
Hakan Altindag <hakangoudberg@hotmail.com>
2019-10-05 13:11:48 +0200

    Added keystore path validator and refactored creation of ssl context

--- client/src/main/java/nl/altindag/client/ClientConfig.java
+++ client/src/main/java/nl/altindag/client/ClientConfig.java
@@ -1,5 +1,11 @@
 package nl.altindag.client;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.HttpClients;
@@ -10,11 +16,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-
 @Configuration
 @Import({SSLTrustManagerHelper.class})
 public class ClientConfig {
@@ -23,16 +24,16 @@ public class ClientConfig {
     private SSLContext sslContext;
 
     @Bean("httpClient")
-    @ConditionalOnProperty(name = "client.ssl.enabled", havingValue = "true")
-    public HttpClient httpClientWithTLS() {
+    @ConditionalOnProperty(name = "client.ssl.mutual-authentication-enabled", havingValue = "true")
+    public HttpClient httpClientWithMutualAuthentication() {
         return HttpClients.custom()
                 .setSSLContext(sslContext)
                 .build();
     }
 
     @Bean("httpClient")
-    @ConditionalOnProperty(name = "client.ssl.enabled", havingValue = "false")
-    public HttpClient httpClientWithoutTLS() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+    @ConditionalOnProperty(name = "client.ssl.mutual-authentication-enabled", havingValue = "false")
+    public HttpClient httpClientWithoutMutualAuthentication() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         return HttpClients.custom()
                 .setSSLContext(SSLContexts.custom()
                         .loadTrustMaterial(null, new TrustSelfSignedStrategy())
