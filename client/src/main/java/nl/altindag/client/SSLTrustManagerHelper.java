35e15c0adc04a8a8494d2ab96413d8fd1890a31c
Hakan Altindag <hakangoudberg@hotmail.com>
2019-10-05 13:11:48 +0200

    Added keystore path validator and refactored creation of ssl context

--- client/src/main/java/nl/altindag/client/SSLTrustManagerHelper.java
+++ client/src/main/java/nl/altindag/client/SSLTrustManagerHelper.java
@@ -1,8 +1,10 @@
 package nl.altindag.client;
 
+import static java.util.Objects.isNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -29,12 +31,12 @@ public class SSLTrustManagerHelper {
     private String trustStore;
     private String trustStorePassword;
 
-    public SSLTrustManagerHelper(@Value("${client.ssl.enabled:false}") boolean sslEnabled,
+    public SSLTrustManagerHelper(@Value("${client.ssl.mutual-authentication-enabled:false}") boolean mutualAuthenticationEnabled,
                                  @Value("${client.ssl.key-store:}") String keyStore,
                                  @Value("${client.ssl.key-store-password:}") String keyStorePassword,
                                  @Value("${client.ssl.trust-store:}") String trustStore,
                                  @Value("${client.ssl.trust-store-password:}") String trustStorePassword) {
-        if (sslEnabled && (isBlank(keyStore) || isBlank(keyStorePassword) || isBlank(trustStore) || isBlank(trustStorePassword))) {
+        if (mutualAuthenticationEnabled && (isBlank(keyStore) || isBlank(keyStorePassword) || isBlank(trustStore) || isBlank(trustStorePassword))) {
             throw new ClientException("TrustStore or KeyStore details are empty, which are required to be present when SSL is enabled");
         }
 
@@ -45,7 +47,7 @@ public class SSLTrustManagerHelper {
     }
 
     @Bean
-    @ConditionalOnProperty(name = "client.ssl.enabled")
+    @ConditionalOnProperty(name = "client.ssl.mutual-authentication-enabled")
     public SSLContext clientSSLContext() {
         try {
             TrustManagerFactory trustManagerFactory = getTrustManagerFactory(trustStore, trustStorePassword);
@@ -65,24 +67,29 @@ public class SSLTrustManagerHelper {
     }
 
     private static KeyManagerFactory getKeyManagerFactory(String keystorePath, String keystorePassword) throws NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException, UnrecoverableKeyException {
-        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-
-        keystore.load(SSLTrustManagerHelper.class.getClassLoader().getResourceAsStream(keystorePath), keystorePassword.toCharArray());
-
+        KeyStore keyStore = loadKeyStore(keystorePath, keystorePassword);
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        keyManagerFactory.init(keystore, keystorePassword.toCharArray());
+        keyManagerFactory.init(keyStore, keystorePassword.toCharArray());
         return keyManagerFactory;
     }
 
-    private static TrustManagerFactory getTrustManagerFactory(String truststorePath, String truststorePassword)
-            throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException {
-        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-
-        trustStore.load(SSLTrustManagerHelper.class.getClassLoader().getResourceAsStream(truststorePath), truststorePassword.toCharArray());
-
+    private static TrustManagerFactory getTrustManagerFactory(String truststorePath, String truststorePassword) throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException {
+        KeyStore trustStore = loadKeyStore(truststorePath, truststorePassword);
         TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         trustManagerFactory.init(trustStore);
         return trustManagerFactory;
+    }
+
+    private static KeyStore loadKeyStore(String keystorePath, String truststorePassword) throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException {
+        try(InputStream keystoreInputStream = SSLTrustManagerHelper.class.getClassLoader().getResourceAsStream(keystorePath)) {
+            if (isNull(keystoreInputStream)) {
+                throw new ClientException(String.format("Could not find the keystore file with the given location %s", keystorePath));
+            }
+
+            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keystore.load(keystoreInputStream, truststorePassword.toCharArray());
+            return keystore;
+        }
     }
 
 }
