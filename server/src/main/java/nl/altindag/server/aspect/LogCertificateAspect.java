b8bb4dcaa198025b4bdabe4a3716bd9562bead57
Hakan Altindag <hakangoudberg@hotmail.com>
2019-11-10 22:24:26 +0100

    Removed library to print the public certificate

--- server/src/main/java/nl/altindag/server/aspect/LogCertificateAspect.java
+++ server/src/main/java/nl/altindag/server/aspect/LogCertificateAspect.java
@@ -1,5 +1,6 @@
 package nl.altindag.server.aspect;
 
+import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Optional;
@@ -7,7 +8,6 @@ import java.util.stream.Stream;
 
 import javax.security.auth.x500.X500Principal;
 
-import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.annotation.Aspect;
@@ -34,14 +34,14 @@ public class LogCertificateAspect {
             certificateDetails = getSubjectDistinguishedName();
         }
 
-        certificateDetails.ifPresent(certificate -> LOGGER.info("Received the following certificate details: " + certificate));
+        certificateDetails.ifPresent(certificate -> LOGGER.info("Received the following certificate details: {}", certificate));
     }
 
     private Optional<String> getPublicCertificate() {
         return getCertificatesFromRequest()
                 .map(Arrays::stream)
                 .flatMap(Stream::findFirst)
-                .map(ConvertUtils::convert);
+                .map(Certificate::toString);
     }
 
     private Optional<String> getSubjectDistinguishedName() {
