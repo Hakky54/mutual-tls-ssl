3c2af318e9ddff161bb6d0608cc7cc817132953c
Hakan Altindag <hakangoudberg@hotmail.com>
2019-11-11 22:45:15 +0100

    Fixed script

--- test-all-scenarios.sh
+++ test-all-scenarios.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-mvn clean verify -P'without-authentication,with-one-way-authentication,with-two-way-authentication-by-trusting-each-other,with-two-way-authentication-by-trusting-root-ca'
+mvn clean verify -P without-authentication && \
+mvn clean verify -P with-one-way-authentication && \
+mvn clean verify -P with-two-way-authentication-by-trusting-each-other && \
+mvn clean verify -P with-two-way-authentication-by-trusting-root-ca
