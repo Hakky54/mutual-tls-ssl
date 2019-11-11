64c37e9e7f7220c760ddc983a81bdece5dede55e
Hakan Altindag <hakangoudberg@hotmail.com>
2019-11-11 22:39:28 +0100

    Added without authentication scenario to the build

--- test-all-scenarios.sh
+++ test-all-scenarios.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean verify && \
-mvn clean verify -P with-one-way-authentication && \
-mvn clean verify -P with-two-way-authentication-by-trusting-each-other && \
-mvn clean verify -P with-two-way-authentication-by-trusting-root-ca
+mvn clean verify -P'without-authentication,with-one-way-authentication,with-two-way-authentication-by-trusting-each-other,with-two-way-authentication-by-trusting-root-ca'
