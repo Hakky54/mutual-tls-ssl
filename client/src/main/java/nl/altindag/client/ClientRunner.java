49be6c18a7e86f6cffcc2a6a8c3891ed54702f32
Hakky54 <hakangoudberg@hotmail.com>
2019-11-27 09:38:09 +0100

    Standalone client as a jar (#19)

---
+++ client/src/main/java/nl/altindag/client/ClientRunner.java
@@ -0,0 +1,22 @@
+package nl.altindag.client;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ClientRunner {
+
+    private static String[] defaultOptions = {
+            "--glue", "nl.altindag.client.stepdefs",
+            "--plugin", "pretty",
+            "classpath:Hello.feature"
+    };
+
+    public static void main(String[] args) {
+        String[] cucumberOptions = Stream.of(defaultOptions, args)
+              .flatMap(Arrays::stream)
+              .toArray(String[]::new);
+
+        io.cucumber.core.cli.Main.main(cucumberOptions);
+    }
+
+}
