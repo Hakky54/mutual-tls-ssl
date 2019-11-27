4abf5cff74a341d3552e1a341d3ed09c6a784f41
Hakan Altindag <hakangoudberg@hotmail.com>
2019-11-27 05:52:58 +0100

    Added build plugin for building a jar, added a main runner class

---
+++ client/src/main/java/nl/altindag/client/PropertyResolver.java
@@ -0,0 +1,23 @@
+package nl.altindag.client;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class PropertyResolver {
+
+    private static final String CLIENT_PROPERTY_FILE = "application.yml";
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource(CLIENT_PROPERTY_FILE));
+        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+}
