e3ffdc1f59fd72faed7df135b48504a218397471
Hakan Altindag <hakangoudberg@hotmail.com>
2019-12-12 16:44:47 +0100

    Updated readme for adjusting server url

--- client/src/main/java/nl/altindag/client/Constants.java
+++ client/src/main/java/nl/altindag/client/Constants.java
@@ -4,7 +4,8 @@ import java.util.Optional;
 
 public final class Constants {
 
-    public static final String SERVER_URL = Optional.ofNullable(System.getProperty("url")).orElse("http://localhost:8080");
+    private static final String DEFAULT_SERVER_URL = "http://localhost:8080";
+    public static final String SERVER_URL = Optional.ofNullable(System.getProperty("url")).orElse(DEFAULT_SERVER_URL);
     public static final String HELLO_ENDPOINT = "/api/hello";
     public static final String HEADER_KEY_CLIENT_TYPE = "client-type";
 
