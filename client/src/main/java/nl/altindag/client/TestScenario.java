1d4dc58354c272ebbbb7d935d6a2a94c68c493fd
Hakan Altindag <hakangoudberg@hotmail.com>
2019-12-22 01:29:46 +0100

    Added additional step to log the execution time of the request for each client type

--- client/src/main/java/nl/altindag/client/TestScenario.java
+++ client/src/main/java/nl/altindag/client/TestScenario.java
@@ -8,6 +8,8 @@ import nl.altindag.client.model.ClientResponse;
 public class TestScenario {
 
     private ClientResponse clientResponse;
+    private long beginTime;
+    private long endTime;
 
     public ClientResponse getClientResponse() {
         return clientResponse;
@@ -15,6 +17,18 @@ public class TestScenario {
 
     public void setClientResponse(ClientResponse clientResponse) {
         this.clientResponse = clientResponse;
+    }
+
+    public void setStartTime(long beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public long getExecutionTimeInMilliSeconds() {
+        return endTime - beginTime;
     }
 
 }
