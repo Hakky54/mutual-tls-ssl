88d594fd4f78be6c632dcec809339d4f3f6e547c
Hakan Altindag <hakangoudberg@hotmail.com>
2020-04-23 12:11:52 +0200

    Added client type header

--- client/src/main/java/nl/altindag/client/service/SttpHttpClientService.scala
+++ client/src/main/java/nl/altindag/client/service/SttpHttpClientService.scala
@@ -5,6 +5,7 @@ import java.net.URI
 import javax.net.ssl.HttpsURLConnection
 import nl.altindag.client.ClientType
 import nl.altindag.client.ClientType._
+import nl.altindag.client.Constants.HEADER_KEY_CLIENT_TYPE
 import nl.altindag.client.model.ClientResponse
 import nl.altindag.sslcontext.SSLFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -26,6 +27,8 @@ class SttpHttpClientService(@Autowired(required = false) sslFactory: SSLFactory)
 
   override def executeRequest(url: String): ClientResponse = {
     val request = basicRequest.get(uri = Uri(javaUri = URI.create(url)))
+                              .header(HEADER_KEY_CLIENT_TYPE, getClientType.getValue)
+
     val response = request.send()
 
     new ClientResponse(response.body.toOption.orNull, response.code.code)
