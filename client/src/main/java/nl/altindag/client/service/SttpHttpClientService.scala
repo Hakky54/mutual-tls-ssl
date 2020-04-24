6fc5bf80e341d58f0bad5494bcee48d7d12cf4c2
Hakan Altindag <hakangoudberg@hotmail.com>
2020-04-24 14:38:25 +0200

    Added unit tests for Sttp

--- client/src/main/java/nl/altindag/client/service/SttpHttpClientService.scala
+++ client/src/main/java/nl/altindag/client/service/SttpHttpClientService.scala
@@ -9,14 +9,33 @@ import nl.altindag.client.Constants.HEADER_KEY_CLIENT_TYPE
 import nl.altindag.client.model.ClientResponse
 import nl.altindag.sslcontext.SSLFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.{Component, Service}
 import sttp.client._
 import sttp.model._
 
 @Service
-class SttpHttpClientService(@Autowired(required = false) sslFactory: SSLFactory) extends RequestService {
+class SttpHttpClientService(sttpBackend: SttpBackend[Identity, Nothing, NothingT]) extends RequestService {
 
-  implicit val backend: SttpBackend[Identity, Nothing, NothingT] = {
+  implicit val backend: SttpBackend[Identity, Nothing, NothingT] = sttpBackend;
+
+  override def executeRequest(url: String): ClientResponse = {
+    val request = basicRequest.get(uri = Uri(javaUri = URI.create(url)))
+                              .header(HEADER_KEY_CLIENT_TYPE, getClientType.getValue)
+
+    val response = request.send()
+    new ClientResponse(response.body.toOption.orNull, response.code.code)
+  }
+
+  override def getClientType: ClientType = STTP
+
+}
+
+@Component
+class SttpHttpClientConfiguration {
+
+  @Bean
+  def createSttpBackendClient(@Autowired(required = false) sslFactory: SSLFactory): SttpBackend[Identity, Nothing, NothingT] = {
     HttpURLConnectionBackend(customizeConnection = {
       case httpsConnection: HttpsURLConnection if sslFactory != null =>
         httpsConnection.setHostnameVerifier(sslFactory.getHostnameVerifier)
@@ -24,16 +43,5 @@ class SttpHttpClientService(@Autowired(required = false) sslFactory: SSLFactory)
       case _ =>
     })
   }
-
-  override def executeRequest(url: String): ClientResponse = {
-    val request = basicRequest.get(uri = Uri(javaUri = URI.create(url)))
-                              .header(HEADER_KEY_CLIENT_TYPE, getClientType.getValue)
-
-    val response = request.send()
-
-    new ClientResponse(response.body.toOption.orNull, response.code.code)
-  }
-
-  override def getClientType: ClientType = STTP
 
 }
---
+++ client/src/test/java/nl/altindag/client/service/SttpHttpClientServiceShould.scala
@@ -0,0 +1,73 @@
+package nl.altindag.client.service
+
+import java.net.URI
+
+import javax.net.ssl.SSLContext
+import nl.altindag.client.TestConstants
+import nl.altindag.client.model.ClientResponse
+import nl.altindag.sslcontext.SSLFactory
+import org.assertj.core.api.Assertions.{assertThat, assertThatThrownBy}
+import org.mockito.scalatest.MockitoSugar
+import org.scalatest.funspec.AnyFunSpec
+import sttp.client.{Identity, NothingT, Request, RequestT, Response, SttpBackend, basicRequest}
+import sttp.model.{StatusCode, Uri}
+
+class SttpHttpClientServiceShould extends AnyFunSpec with MockitoSugar {
+
+  describe("execute request") {
+    val mockedBackend = mock[SttpBackend[Identity, Nothing, NothingT]]
+    val mockedResponse = mock[Identity[Response[Either[String, String]]]]
+    val mockedBody = mock[Either[String, String]]
+
+    when(mockedBackend.send(any[RequestT[Identity, Either[String, String], Nothing]])).thenReturn(mockedResponse)
+    when(mockedResponse.code).thenReturn(StatusCode.Ok)
+    when(mockedResponse.body).thenReturn(mockedBody)
+    when(mockedBody.toOption).thenReturn(Option.apply("Hello"))
+
+    val victim = new SttpHttpClientService(mockedBackend)
+    val clientResponse: ClientResponse = victim.executeRequest(TestConstants.HTTP_URL)
+
+    assertThat(clientResponse.getStatusCode).isEqualTo(200)
+    assertThat(clientResponse.getResponseBody).isEqualTo("Hello")
+  }
+
+  describe("create Sttp backend client without ssl") {
+    val victim: SttpBackend[Identity, Nothing, NothingT] = new SttpHttpClientConfiguration().createSttpBackendClient(null)
+    assertThat(victim).isNotNull
+  }
+
+  describe("create Sttp backend client with ssl") {
+    val sslFactory = mock[SSLFactory]
+    val sslContext = mock[SSLContext]
+
+    when(sslFactory.getSslContext).thenReturn(sslContext)
+
+    val victim: SttpBackend[Identity, Nothing, NothingT] = new SttpHttpClientConfiguration().createSttpBackendClient(sslFactory)
+    val request: Request[Either[String, String], Nothing] =  basicRequest.get(uri = Uri(javaUri = URI.create(TestConstants.HTTPS_URL)))
+
+    when(sslFactory.getSslContext).thenReturn(sslContext)
+
+    assertThat(victim).isNotNull
+    assertThatThrownBy(() => victim.send(request))
+
+    verify(sslFactory, times(1)).getHostnameVerifier
+    verify(sslFactory, times(1)).getSslContext
+    verify(sslContext, times(1)).getSocketFactory
+  }
+
+  describe("create Sttp backend client without ssl when url is http scheme") {
+    val sslFactory = mock[SSLFactory]
+    val sslContext = mock[SSLContext]
+
+    val victim: SttpBackend[Identity, Nothing, NothingT] = new SttpHttpClientConfiguration().createSttpBackendClient(sslFactory)
+    val request: Request[Either[String, String], Nothing] =  basicRequest.get(uri = Uri(javaUri = URI.create(TestConstants.HTTP_URL)))
+
+    assertThat(victim).isNotNull
+    assertThatThrownBy(() => victim.send(request))
+
+    verify(sslFactory, times(0)).getHostnameVerifier
+    verify(sslFactory, times(0)).getSslContext
+    verify(sslContext, times(0)).getSocketFactory
+  }
+
+}
