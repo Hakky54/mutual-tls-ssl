cbe2b8f3c0e70172ca06b9011101f5dc1ad3a946
Hakan Altindag <hakangoudberg@hotmail.com>
2020-04-26 00:05:05 +0200

    Added unit test

--- client/src/test/java/nl/altindag/client/service/ScalaJHttpClientServiceShould.java
+++ client/src/test/java/nl/altindag/client/service/ScalaJHttpClientServiceShould.java
@@ -2,6 +2,7 @@ package nl.altindag.client.service;
 
 import nl.altindag.client.model.ClientResponse;
 import nl.altindag.sslcontext.SSLFactory;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,12 +13,20 @@ import scala.Function1;
 import scalaj.http.HttpRequest;
 import scalaj.http.HttpResponse;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+
 import static nl.altindag.client.ClientType.SCALAJ_HTTP_CLIENT;
 import static nl.altindag.client.Constants.HEADER_KEY_CLIENT_TYPE;
+import static nl.altindag.client.TestConstants.HTTPS_URL;
 import static nl.altindag.client.TestConstants.HTTP_URL;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SuppressWarnings("unchecked")
@@ -29,6 +38,14 @@ public class ScalaJHttpClientServiceShould {
     private ScalaJHttpClientService victim;
     @Mock
     private SSLFactory sslFactory;
+
+    @Before
+    public void setUp() {
+        SSLContext mockedSslContext = mock(SSLContext.class);
+        when(mockedSslContext.getSocketFactory()).thenReturn(mock(SSLSocketFactory.class));
+        when(sslFactory.getSslContext()).thenReturn(mockedSslContext);
+        when(sslFactory.getHostnameVerifier()).thenReturn(mock(HostnameVerifier.class));
+    }
 
     @Test
     public void executeRequest() throws Exception {
@@ -47,6 +64,18 @@ public class ScalaJHttpClientServiceShould {
 
         assertThat(clientResponse.getStatusCode()).isEqualTo(200);
         assertThat(clientResponse.getResponseBody()).isEqualTo("Hello");
+    }
+
+    @Test
+    public void executeHttpsRequest() {
+        // only validating if the ssl materials will be used for a https request
+        // the client is not mocked so it will try to execute the request which
+        // will result into an exception
+        assertThatThrownBy(() -> victim.executeRequest(HTTPS_URL));
+
+        verify(sslFactory, times(1)).getSslContext();
+        verify(sslFactory, times(1)).getHostnameVerifier();
+        verify(sslFactory.getSslContext(), times(1)).getSocketFactory();
     }
 
 }
