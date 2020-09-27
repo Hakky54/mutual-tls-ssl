7d1f4af7fe834a171fbe8a6a5a8f8c42bbeeb551
Hakan Altindag <hakangoudberg@hotmail.com>
2020-09-27 23:17:45 +0200

    Fixed unirest test

--- client/src/test/java/nl/altindag/client/service/UnirestServiceShould.java
+++ client/src/test/java/nl/altindag/client/service/UnirestServiceShould.java
@@ -1,82 +1,34 @@
 package nl.altindag.client.service;
 
-import static nl.altindag.client.ClientType.UNIREST;
-import static nl.altindag.client.TestConstants.HEADER_KEY_CLIENT_TYPE;
-import static nl.altindag.client.TestConstants.HTTP_URL;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.message.BasicHeader;
+import kong.unirest.Unirest;
+import nl.altindag.client.model.ClientResponse;
+import nl.altindag.client.util.MockServerTestHelper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpMethod;
 
-import kong.unirest.Unirest;
-import kong.unirest.apache.ApacheClient;
-import nl.altindag.client.model.ClientResponse;
+import static nl.altindag.client.ClientType.UNIREST;
+import static nl.altindag.client.TestConstants.HTTP_URL;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("SameParameterValue")
-@ExtendWith(MockitoExtension.class)
 class UnirestServiceShould {
 
     private UnirestService victim;
-    private HttpClient httpClient;
 
     @BeforeEach
     void setUp() {
         victim = new UnirestService();
-
-        httpClient = mock(HttpClient.class);
-        Unirest.config()
-               .httpClient(config -> ApacheClient.builder(httpClient).apply(config));
     }
 
     @Test
-    void executeRequest() throws IOException {
-        mockUnirest("Hello", 200);
+    void executeRequest() {
+        MockServerTestHelper.mockResponseForClient(UNIREST);
 
-        ArgumentCaptor<HttpRequestBase> httpRequestBaseArgumentCaptor = ArgumentCaptor.forClass(HttpRequestBase.class);
         ClientResponse clientResponse = victim.executeRequest(HTTP_URL);
 
         assertThat(clientResponse.getStatusCode()).isEqualTo(200);
         assertThat(clientResponse.getResponseBody()).isEqualTo("Hello");
-
-        verify(httpClient, times(1)).execute(httpRequestBaseArgumentCaptor.capture());
-        assertThat(httpRequestBaseArgumentCaptor.getValue().getURI()).hasToString(HTTP_URL);
-        assertThat(httpRequestBaseArgumentCaptor.getValue().getFirstHeader(HEADER_KEY_CLIENT_TYPE).getValue()).isEqualTo(UNIREST.getValue());
-        assertThat(httpRequestBaseArgumentCaptor.getValue().getMethod()).isEqualTo(HttpMethod.GET.toString());
-    }
-
-    private void mockUnirest(String body, int statusCode) throws IOException {
-        HttpResponse httpResponse = mock(HttpResponse.class);
-        StatusLine statusLine = mock(StatusLine.class);
-        HttpEntity httpEntity = mock(HttpEntity.class);
-
-        when(httpResponse.getStatusLine()).thenReturn(statusLine);
-        when(statusLine.getReasonPhrase()).thenReturn(EMPTY);
-        when(statusLine.getStatusCode()).thenReturn(statusCode);
-        when(httpClient.execute(any())).thenReturn(httpResponse);
-        when(httpResponse.getEntity()).thenReturn(httpEntity);
-        when(httpEntity.getContent()).thenReturn(new ByteArrayInputStream(body.getBytes()));
-        when(httpResponse.getAllHeaders()).thenReturn(new Header[]{new BasicHeader(EMPTY, EMPTY)});
     }
 
     @AfterEach
