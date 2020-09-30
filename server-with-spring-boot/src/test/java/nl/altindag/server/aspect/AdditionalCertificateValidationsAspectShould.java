8c627a084b32d9005575a5f30c3319b08922b281
Hakan Altindag <hakangoudberg@hotmail.com>
2020-10-01 00:25:51 +0200

    Added additional unit test to cover other branches

--- server-with-spring-boot/src/test/java/nl/altindag/server/aspect/AdditionalCertificateValidationsAspectShould.java
+++ server-with-spring-boot/src/test/java/nl/altindag/server/aspect/AdditionalCertificateValidationsAspectShould.java
@@ -13,6 +13,8 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.security.auth.x500.X500Principal;
 import java.lang.annotation.Annotation;
 import java.security.cert.X509Certificate;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -29,7 +31,28 @@ class AdditionalCertificateValidationsAspectShould {
     @Test
     void validateCertificateForAllowedCommonNameContinuesTheFlowWithProceedingJoinPoint() throws Throwable {
         var proceedingJoinPoint = mock(ProceedingJoinPoint.class);
-        var additionalCertificateValidations = createAdditionalCertificateValidations();
+        var additionalCertificateValidations = createAdditionalCertificateValidations(List.of("black-hole"), List.of("white-tower"));
+
+        X509Certificate x509Certificate = mock(X509Certificate.class);
+        X509Certificate[] x509Certificates = new X509Certificate[]{x509Certificate};
+        X500Principal x500Principal = mock(X500Principal.class);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setAttribute("javax.servlet.request.X509Certificate", x509Certificates);
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        when(x509Certificate.getSubjectX500Principal()).thenReturn(x500Principal);
+        when(x500Principal.getName()).thenReturn("CN=black-hole,OU=Altindag,O=Altindag,C=NL");
+
+        victim.validate(proceedingJoinPoint, additionalCertificateValidations);
+
+        verify(proceedingJoinPoint, times(1)).proceed();
+    }
+
+    @Test
+    void validateCertificateForEmptyListOfAllowedAndNotAllowedCommonNameContinuesTheFlowWithProceedingJoinPoint() throws Throwable {
+        var proceedingJoinPoint = mock(ProceedingJoinPoint.class);
+        var additionalCertificateValidations = createAdditionalCertificateValidations(Collections.emptyList(), Collections.emptyList());
 
         X509Certificate x509Certificate = mock(X509Certificate.class);
         X509Certificate[] x509Certificates = new X509Certificate[]{x509Certificate};
@@ -50,7 +73,33 @@ class AdditionalCertificateValidationsAspectShould {
     @Test
     void validateCertificateForNotAllowedCommonNameReturnsBadRequestHttpStatus() throws Throwable {
         var proceedingJoinPoint = mock(ProceedingJoinPoint.class);
-        var additionalCertificateValidations = createAdditionalCertificateValidations();
+        var additionalCertificateValidations = createAdditionalCertificateValidations(List.of("black-hole"), List.of("white-tower"));
+
+        X509Certificate x509Certificate = mock(X509Certificate.class);
+        X509Certificate[] x509Certificates = new X509Certificate[]{x509Certificate};
+        X500Principal x500Principal = mock(X500Principal.class);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setAttribute("javax.servlet.request.X509Certificate", x509Certificates);
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        when(x509Certificate.getSubjectX500Principal()).thenReturn(x500Principal);
+        when(x500Principal.getName()).thenReturn("CN=white-tower,OU=Altindag,O=Altindag,C=NL");
+
+        Object response = victim.validate(proceedingJoinPoint, additionalCertificateValidations);
+
+        verify(proceedingJoinPoint, times(0)).proceed();
+        assertThat(response).isInstanceOf(ResponseEntity.class);
+
+        ResponseEntity responseEntity = (ResponseEntity) response;
+        assertThat(responseEntity.getStatusCode().value()).isEqualTo(400);
+        assertThat(responseEntity.getBody()).isEqualTo("This certificate is not a valid one");
+    }
+
+    @Test
+    void validateCertificateForNotAllowedCommonNameReturnsBadRequestHttpStatusEvenWhenAllowedListIsEmpty() throws Throwable {
+        var proceedingJoinPoint = mock(ProceedingJoinPoint.class);
+        var additionalCertificateValidations = createAdditionalCertificateValidations(Collections.emptyList(), List.of("white-tower"));
 
         X509Certificate x509Certificate = mock(X509Certificate.class);
         X509Certificate[] x509Certificates = new X509Certificate[]{x509Certificate};
@@ -76,7 +125,7 @@ class AdditionalCertificateValidationsAspectShould {
     @Test
     void validateCertificateForUnrecognizedCommonNameReturnsBadRequestHttpStatus() throws Throwable {
         var proceedingJoinPoint = mock(ProceedingJoinPoint.class);
-        var additionalCertificateValidations = createAdditionalCertificateValidations();
+        var additionalCertificateValidations = createAdditionalCertificateValidations(List.of("black-hole"), List.of("white-tower"));
 
         X509Certificate x509Certificate = mock(X509Certificate.class);
         X509Certificate[] x509Certificates = new X509Certificate[]{x509Certificate};
@@ -104,7 +153,7 @@ class AdditionalCertificateValidationsAspectShould {
         LogCaptor logCaptor = LogCaptor.forClass(AdditionalCertificateValidationsAspect.class);
 
         var proceedingJoinPoint = mock(ProceedingJoinPoint.class);
-        var additionalCertificateValidations = createAdditionalCertificateValidations();
+        var additionalCertificateValidations = createAdditionalCertificateValidations(Collections.emptyList(), Collections.emptyList());
 
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
@@ -115,7 +164,7 @@ class AdditionalCertificateValidationsAspectShould {
         assertThat(logCaptor.getInfoLogs()).containsExactly("Skipping common name validation because certificate is not present within the request");
     }
 
-    private AdditionalCertificateValidations createAdditionalCertificateValidations() {
+    private AdditionalCertificateValidations createAdditionalCertificateValidations(List<String> allowedCommonNames, List<String> notAllowedCommonNames) {
         return new AdditionalCertificateValidations() {
 
             @Override
@@ -125,12 +174,12 @@ class AdditionalCertificateValidationsAspectShould {
 
             @Override
             public String[] allowedCommonNames() {
-                return new String[]{"black-hole"};
+                return allowedCommonNames.toArray(new String[0]);
             }
 
             @Override
             public String[] notAllowedCommonNames() {
-                return new String[]{"white-tower"};
+                return notAllowedCommonNames.toArray(new String[0]);
             }
         };
     }
