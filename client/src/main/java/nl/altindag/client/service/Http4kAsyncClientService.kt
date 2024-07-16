293cfb8fbebe87a1b62d4fdbbda21d997b9b6770
Hakky54 <hakangoudberg@hotmail.com>
2024-07-16 14:44:37 +0200

    Bumped http4k, jetty reactive httpclient and logback

--- client/src/main/java/nl/altindag/client/service/Http4kAsyncClientService.kt
+++ client/src/main/java/nl/altindag/client/service/Http4kAsyncClientService.kt
@@ -18,12 +18,12 @@ package nl.altindag.client.service
 import nl.altindag.client.Constants
 import nl.altindag.client.model.ClientResponse
 import org.awaitility.Awaitility.await
-import org.http4k.client.AsyncHttpClient
+import org.http4k.client.AsyncHttpHandler
 import org.http4k.core.Method
 import org.http4k.core.Request
 import java.util.concurrent.TimeUnit
 
-abstract class Http4kAsyncClientService(val client: AsyncHttpClient): RequestService {
+abstract class Http4kAsyncClientService(val client: AsyncHttpHandler): RequestService {
 
     override fun executeRequest(url: String): ClientResponse {
         var response: ClientResponse? = null
